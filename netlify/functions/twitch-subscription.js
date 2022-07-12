06563a5ebdef4a1821d6134368f3a6c220d85bde
how2bepic-admin <marshallleebusiness@gmail.com>
2022-07-11 23:09:14 -0400

    show subscriptions

--- netlify/functions/twitch-subscription.js
+++ netlify/functions/twitch-subscription.js
@@ -1,13 +1,27 @@
 exports.handler = async (event) => {
     const { headers = [] } = event;
 
-    const type = headers['twitch_eventsub-message-type'] || 'no type';
+    const type = headers['twitch-eventsub-message-type'] || 'no type';
+    const eventType = headers['twitch-eventsub-subscription-type'];
+
+
+    if (type !== 'notification' || eventType !== 'channel.subscribe') {
+        return { statusCode: 200, body: '' };
+    }
+    
+    
+    console.log({ headers: event.headers, body: event.body} )
+
+    // Requests are now valid.
+    // Insert code here if you want to get fun with it.
+
+    const { event: twitchEvent } = JSON.parse(event.body);
+    const user = twitchEvent.user_name
+
+    console.log('${user} just subscribed!');
 
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            type,
-            message: 'ok',
-        }),
+        body: '',
     };
 };
